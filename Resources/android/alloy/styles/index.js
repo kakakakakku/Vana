0fc49a408678c617070850a4325a079311fb4ac0
kakakakakku <y.yoshida22@gmail.com>
2014-02-11 03:11:30 +0900

    登録時のUIにカテゴリーを追加

---
+++ Resources/android/alloy/styles/index.js
@@ -0,0 +1,1 @@
+module.exports = [{"isApi":true,"priority":1000.0001,"key":"Window","style":{backgroundColor:"#FFFFFF",}},{"isApi":true,"priority":1000.0025,"key":"Window","style":{backgroundColor:"#FFFFFF",}}];
